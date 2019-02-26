a2e169baef19843108d19c8f4263a5dffa184285
Bq Lion <398703917@qq.com>
2019-02-26 21:11:57 +0800

    feat: test guava

---
+++ src/main/java/com/bqlion/study/TestMain.java
@@ -0,0 +1,15 @@
+package com.bqlion.study;
+
+import com.google.common.base.Strings;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        String input = "";
+        boolean isNullOrEmpty = Strings.isNullOrEmpty(input);
+        System.out.println(isNullOrEmpty);
+
+    }
+
+
+}
