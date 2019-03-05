74ad96b5fbc303e35804c94e0ab124a7a36f621d
Bq Lion <398703917@qq.com>
2019-03-05 16:10:17 +0800

    添加计算器程序

--- src/main/java/com/bqlion/study/TestMain.java
+++ src/main/java/com/bqlion/study/TestMain.java
@@ -2,14 +2,16 @@ package com.bqlion.study;
 
 import com.google.common.base.Strings;
 
+import static net.mindview.util.Print.print;
+
 public class TestMain {
 
     public static void main(String[] args) {
-        String input = "";
+        String input = "5+(10/2)-7*(2*6)#";
         boolean isNullOrEmpty = Strings.isNullOrEmpty(input);
-        System.out.println(isNullOrEmpty);
-
-        StringBuilder stringBuilder = new StringBuilder();
+        print(isNullOrEmpty);
+        Calculator calculator = new Calculator();
+        print(calculator.calc(input));
     }
 
 
