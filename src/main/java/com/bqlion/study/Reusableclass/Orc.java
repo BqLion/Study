6b5cfc26b705aa2dd8ef901b86163c0bc3bbb518
Bq Lion <398703917@qq.com>
2019-03-08 22:08:56 +0800

    update to Chapter 6.8 final

---
+++ src/main/java/com/bqlion/study/Reusableclass/Orc.java
@@ -0,0 +1,22 @@
+package com.bqlion.study.Reusableclass;
+//The Protected keyword
+import java.util.*;
+
+class Villain{
+    private int i;
+    protected int read(){return i;}
+    protected void set(int ii){i = ii;}
+    public Villain(int ii){i = ii;}
+    public int value(int m){return m*i; }
+}
+public class Orc extends Villain{
+    private int j;
+    public Orc(int jj){super (jj); j = jj;}
+    public void change(int x){set(x);}
+}
+    /*      类内部  本包  子类  外部包
+public      Y       Y     Y     Y
+protected   Y       Y     Y     N
+default     Y       Y     N     N
+private     Y       N     N     N
+     */
