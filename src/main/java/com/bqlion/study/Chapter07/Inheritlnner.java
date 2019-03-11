42e5f3c2ba154d07f01dde50820c217bd1d6d836
Bq Lion <398703917@qq.com>
2019-03-11 22:20:52 +0800

    Chapter 07 内部类part2

---
+++ src/main/java/com/bqlion/study/Chapter07/Inheritlnner.java
@@ -0,0 +1,17 @@
+package com.bqlion.study.Chapter07;
+
+class WithInner{
+    class Inner{}
+}
+
+public class Inheritlnner
+    extends WithInner.Inner{      //继承内部类就是 extends 外部类.内部类
+    Inheritlnner(WithInner wi){     //对内部类的扩展
+        wi.super();
+    }
+
+    public static void main(String[] args) {
+        WithInner wi = new WithInner();
+        Inheritlnner ii  = new Inheritlnner(wi);
+    }
+}
