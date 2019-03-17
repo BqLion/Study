5f01261ab0a746ecaef579634e34d5cc348aa1f1
Bq Lion <398703917@qq.com>
2019-03-17 11:42:28 +0800

    泛型

--- src/main/java/com/bqlion/study/Chapter09/Coffee/CoffeeGnerator.java
+++
@@ -1,4 +0,0 @@
-package com.bqlion.study.Chapter09.Coffee;
-
-public class CoffeeGnerator {
-}
---
+++ src/main/java/com/bqlion/study/Chapter09/Coffee/Generators.java
@@ -0,0 +1,20 @@
+package com.bqlion.study.Chapter09.Coffee;
+import java.util.*;
+import net.mindview.util.*;
+
+public class Generators {
+    public static<T> Collection<T>
+    fill(Collection<T>coll,Generator<T>gen,int n){
+        coll.add(gen.next());
+        return coll;
+    }
+
+    public static void main(String[] args) {
+        Collection<coffee> coffee = fill(new ArrayList<coffee>(),new CoffeeGenerator(),4);
+        for(coffee c : coffee)
+        System.out.println(c);
+        Collection<Integer>fnumbers = fill(new ArrayList<Integer>(), new Fibonacci(), 12);
+        for(int i: fnumbers)
+            System.out.print(i + ", ");
+    }
+}
