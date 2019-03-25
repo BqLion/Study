3894369fb4d8e57ecdad4ee2819a4dc350b2ba0a
Bq Lion <398703917@qq.com>
2019-03-25 15:40:51 +0800

    数据结构值线性栈

--- src/main/java/com/bqlion/DataStructure/Stack/SeqStack.java
+++ src/main/java/com/bqlion/DataStructure/Stack/SeqStack.java
@@ -21,12 +21,37 @@ public class SeqStack<E>implements Stack<E> {
     public boolean push(E element){
         if(element == null)             //首先插入元素不可以是null
             return false;
-        if(this.top = value.length -1 ) {    //如果栈顶元素的下表是数组的长度减一，代表数组容量已满，需要扩充容量。
+        if(this.top == value.length -1 ) {
+                                             //如果栈顶元素的下标是数组的长度减一，代表数组容量已满，需要扩充容量。
                                              //下标是 0 1 2 3 4，数组长度是1,2,3,4,5
                                              //object[].length是可以直接调用的计算数组长度的函数。
-
-
-        }
-
+        Object temp[] = this.value;
+        this.value = new Object[temp.length * 2];
+        for(int i = 0; i < temp.length; i++)
+            this.value[i] = temp[i];
+        }                                       //以上一个if是如果栈满扩容的操作。
+        this.top++;
+        this.value[this.top] = element;
+        return true;                        //这三句是正常的压栈程序
     }
+
+    public E pop(){
+        if(!isEmpty())
+            return (E)this.value[this.top--];   //弹栈，直接返回top指针所指，然后top--
+        else
+            return null;
+    }
+
+    public E get(){
+        if(!isEmpty())
+            return (E)this.value[this.top--];
+        else
+            return null;
+    }
+
+    public void show(){
+        for(int i = 0; i <= this.top; i++)
+            System.out.println(this.value[i]);
+    }
+
 }
